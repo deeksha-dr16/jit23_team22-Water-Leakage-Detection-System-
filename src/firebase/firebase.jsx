66267927b3a1e10f6a542f91ab2fc31fa0d90980
Deeksha <deeksha-dr16>
2026-07-30 04:57:56 +0530

    Updated SomeComponent.jsx

--- src/firebase/firebase.jsx
+++ src/firebase/firebase.jsx
@@ -7,14 +7,14 @@ import { getDatabase } from "firebase/database";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDB0LUVYs_yy7k6KhY2Q1Nez8_8hFL0Jjs",
-  authDomain: "leakagedetectionsystem-e7e9f.firebaseapp.com",
-  databaseURL: "https://leakagedetectionsystem-e7e9f-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "leakagedetectionsystem-e7e9f",
-  storageBucket: "leakagedetectionsystem-e7e9f.firebasestorage.app",
-  messagingSenderId: "64561764365",
-  appId: "1:64561764365:web:cf31c06b275e1f97a31d44",
-  measurementId: "G-KTTL2SG93Z"
+  apiKey: "enter_ your_details",
+  authDomain: "enter_ your_details",
+  databaseURL: "enter_ your_details",
+  projectId: "enter_ your_details",
+  storageBucket: "enter_ your_details",
+  messagingSenderId: "enter_ your_details",
+  appId: "enter_ your_details",
+  measurementId: "enter_ your_details"
 };
 
 const app = initializeApp(firebaseConfig);
